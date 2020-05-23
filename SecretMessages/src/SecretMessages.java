e8499c8f953279099d3e6ae8aebd4e7b3b42e4a7
Kola Ra <santaramus@gmail.com>
2020-05-23 15:41:21 +0300

    commit version0.1

--- SecretMessages/src/SecretMessages.java
+++ SecretMessages/src/SecretMessages.java
@@ -5,6 +5,41 @@ public class SecretMessages {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter a message to encode or decode: ");
 		String message = scanner.nextLine();
+		String output = "";
+		System.out.println("Enter a secret key(-25 to 25):");
+		int keyValue = Integer.parseInt(scanner.nextLine());
+		char key = (char) keyValue;
+		for (int x = 0; x < message.length(); x++) {
+			char input = message.charAt(x);
+			if (input >= 'A' && input <= 'Z') {
+				input += key;
+				if (input > 'Z') {
+					input -= 26;
+				}
+				if (input < 'A') {
+					input += 26;
+				}
+			} else if (input >= 'a' && input <= 'z') {
+				input += key;
+				if (input > 'z') {
+					input -= 26;
+				}
+				if (input < 'a') {
+					input += 26;
+				}
+			} else if (input >= '0' && input <= '9') {
+				input += (keyValue % 10);
+				if (input > '9') {
+					input -= 10;
+				}
+				if (input < '0') {
+					input += 10;
+				}
+			}
+			output += input;
+		}
+		System.out.println(output);
+		scanner.close();
 	}
 
 }
