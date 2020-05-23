e07740b776c139a4208be8df5a4629c8f24680f3
Kola Ra <santaramus@gmail.com>
2020-05-23 13:23:22 +0300

    first commit

---
+++ SecretMessages/src/SecretMessages.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class SecretMessages {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a message to encode or decode: ");
+		String message = scanner.nextLine();
+	}
+
+}
